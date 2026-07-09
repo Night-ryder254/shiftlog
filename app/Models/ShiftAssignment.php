336aaa3cb35b34856a86822d5d187175b2b0107f
Night-ryder254 <nigelmatekwa@gmail.com>
2026-07-09 19:48:33 +0300

    feat: add database schema for branches, departments, employees, shifts, attendance

---
+++ app/Models/ShiftAssignment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShiftAssignment extends Model
+{
+    /** @use HasFactory<\Database\Factories\ShiftAssignmentFactory> */
+    use HasFactory;
+}
